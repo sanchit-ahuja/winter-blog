04fec4dd3c929b6eeb49a71ffe5621a49f0af12d
Hemanth V. Alluri <hdrive1999@gmail.com>
2018-01-08 10:59:22 +0530

    HYPRO999: login Bug fixed, and some of the code has been cleaned up.

--- blog/winterblog/forms.py
+++ blog/winterblog/forms.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.forms import ModelForm
 from .models import Comment
 
+
 class SignUpForm(UserCreationForm):
     first_name=forms.CharField(max_length=30)
     last_name=forms.CharField(max_length=30)
@@ -12,14 +13,17 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+
+
 class LoginForm(forms.Form):
     username=forms.CharField(max_length=30)
     password=forms.CharField(max_length=30,widget=forms.PasswordInput)
     class Meta:
         model=User
         fields=('username','password')
+
+
 class CommentForm(ModelForm):
     class Meta:
         model=Comment
         fields=('blogger','comment_text',)
-        
